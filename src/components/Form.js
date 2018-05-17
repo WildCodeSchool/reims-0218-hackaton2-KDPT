7ba61b93d93754c0fa7b5433cc74967c28b2ca38
Thib <thibaud.royer051@gmail.com>
2018-05-17 15:02:35 +0200

    add story book and creat three select in from

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,57 @@
-import React from 'react'
+import React from "react";
 
-const Form = () => <div>Form </div>
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "feature" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  handleSubmit(event) {
+    alert("Your favorite flavor is: " + this.state.value);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Choose the gender :
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </label>
+        <label>
+          Choose the Hair Color :
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="blond">Blond</option>
+            <option value="brown">Brown</option>
+            <option value="black">Black</option>
+            <option value="light brown">Light Brown</option>
+            <option value="auburn">Auburn</option>
+            <option value="white">White</option>
+            <option value="red">Red</option>
+            <option value="Gold">gold</option>
+          </select>
+        </label>
+        <label>
+          Choose the eye Color :
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="blue">blue</option>
+            <option value="yellow">yellow</option>
+            <option value="light brown">Light Brown</option>
+            <option value="red">red</option>
+            <option value="brown">brown</option>
+            <option value="hazel">hazel</option>
+            <option value="gray-blue">gray-blue</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
 
-export default Form
+export default Form;
