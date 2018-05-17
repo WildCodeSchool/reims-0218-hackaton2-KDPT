72aacef14614917a3ece31246f724cd578386354
Thib <thibaud.royer051@gmail.com>
2018-05-17 16:02:36 +0200

    gender and hair ok

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,15 +3,18 @@ import React from "react";
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "feature" };
-    this.handleChangeGender = this.handleChangeGender.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      gender: "female",
+      hair: "black"
+    };
+    this.handleGenderChange = this.handleGenderChange.bind(this);
+    this.handleHairChange = this.handleHairChange.bind(this);
   }
-  handleChangeGender(event) {
-    this.setState({ value: event.target.value });
+  handleGenderChange(event) {
+    this.setState({ gender: event.target.value });
   }
-  handleChange(event) {
-    this.setState({ value: event.target.hair });
+  handleHairChange(event) {
+    this.setState({ hair: event.target.value });
   }
 
   render() {
@@ -19,22 +22,22 @@ class Form extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Choose the gender :
-          <select value={this.state.value} onChange={this.handleChangeGender}>
+          <select value={this.state.gender} onChange={this.handleGenderChange}>
             <option value="male">Male</option>
             <option value="female">Female</option>
           </select>
         </label>
         <label>
           Choose the Hair Color :
-          <select hair={this.state.value} onChange={this.handleChange}>
-            <option hair="blond">Blond</option>
-            <option hair="brown">Brown</option>
-            <option hair="black">Black</option>
-            <option hair="light brown">Light Brown</option>
-            <option hair="auburn">Auburn</option>
-            <option hair="white">White</option>
-            <option hair="red">Red</option>
-            <option hair="Gold">gold</option>
+          <select value={this.state.hair} onChange={this.handleHairChange}>
+            <option value="blond">Blond</option>
+            <option value="brown">Brown</option>
+            <option value="black">Black</option>
+            <option value="light brown">Light Brown</option>
+            <option value="auburn">Auburn</option>
+            <option value="white">White</option>
+            <option value="red">Red</option>
+            <option value="Gold">gold</option>
           </select>
         </label>
         {/* <label>
