5322636cf9fdae14ccd5d36d84ee127905fa0331
Pierre <dumaypierre@outlook.fr>
2018-05-17 13:38:49 +0200

    add reactsrap

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-
-import { Button } from '@storybook/react/demo';
 
 storiesOf('Home anim', module).add('Button Search', () => (
   <Button onClick={action('clicked')}>
